ed1beae95e5cb32e0a8ff1ef190954ba202c7a2b
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-14 14:05:45 -0800

    refactor: moving config centered lua to module

---
+++ lua/config/telescope.lua
@@ -0,0 +1,5 @@
+
+require("telescope").load_extension "file_browser"
+require("telescope").load_extension "lazy"
+require("telescope").load_extension "project"
+require("telescope").load_extension "frecency"
