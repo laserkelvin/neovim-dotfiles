beda14c93e0f1c6d40ff85ebdf47953b2cb6dc59
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-14 14:34:49 -0800

    fix: updating doge build

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -5,5 +5,5 @@ return {
 	"neomake/neomake",
 	"kylechui/nvim-surround",
 	{"windwp/nvim-autopairs", enable_check_bracket_line = false, ignored_nexty_char = "[%w%.]"},
-	{"kkoomen/vim-doge", build = "npm -i --no-save && npm run build:binary:unix"},
+	{"kkoomen/vim-doge", build = ":call doge#install()"},
 }
