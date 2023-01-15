58cb55de53d1f8740d0bc98ea1278fedfd3877cd
Kin Long Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-14 22:41:43 -0800

    plugin: added startup plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,4 +3,5 @@ return {
 	"romgrk/barbar.nvim",
 	"kyazdani42/nvim-web-devicons",
 	{"nvim-lualine/lualine.nvim", options = { icons_enabled = false, theme = "auto"}},
+	{"mhinz/vim-startify", branch = "master"}
 }
