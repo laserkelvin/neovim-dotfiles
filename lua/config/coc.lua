1369909adab0d0412b29cc27769141d0281cc8db
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-15 10:36:37 -0800

    bindings: adding show docs with Coc

--- lua/config/coc.lua
+++ lua/config/coc.lua
@@ -1,3 +1,23 @@
 
 -- This should in theory make CoC install extensions automatically
 vim.g['coc_global_extensions'] = ['coc-pyright', 'coc-json']
+
+function _G.show_docs()
+	local cw = vim.fn.expand("<cword>")
+	if vim.fn.index({"vim", "help"}, vim.bo.filetype) >= 0 then
+		vim.api.nvim_command("h " .. cw)
+	elseif vim.api.nvim_eval("coc#rpc#ready()") then
+		vim.fn.CocActionAsync("doHover")
+	else
+		vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
+	end
+end
+
+vim.keymap.set("n", "K", "<CMD>lua _G.show_docs()<CR>", { silent = true})
+
+vim.api.nvim_create_autgroup("CocGroup", {})
+vim.api.nvim_create_autocmd("CursorHold", {
+	group = "CocGroup",
+	command = "silent call CocActionAsync('highlight')",
+	desc = "Highlight symbol under cursor on CursorHold"
+})
