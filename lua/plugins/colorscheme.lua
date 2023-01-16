5510b8e3b6a3e51db4211eaf1b599fe9790dee89
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2023-01-15 16:28:38 -0800

    plugin: added folke zenmode plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,8 @@
 return {
 	"ellisonleao/gruvbox.nvim",
 	{"nvim-treesitter/nvim-treesitter", tag = "v0.8.1"},
+	{"folke/zen-mode.nvim", config = function()
+	    require("zen-mode").setup {}
+	  end
+  },
 }
