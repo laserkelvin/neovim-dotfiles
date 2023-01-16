79fa5fa367b1316c611ded9af0561179e7f27e26
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2023-01-15 16:39:55 -0800

    config: ostensibly points to a Python version

--- lua/config/languages.lua
+++ lua/config/languages.lua
@@ -8,3 +8,7 @@ vim.g['vim_markdown_math'] = 1
 vim.g['vim_markdown_frontmatter'] = 1
 vim.g['vim_markdown_toml_frontmatter'] = 1
 vim.g['vim_markdown_json_frontmatter'] = 1
+
+-- Python settings
+-- Inevitably, the host Python program will need to be changed
+vim.g["python3_host_prog"] = "/home/kinlong/miniconda3/bin/python"
