0c562a22e551434351d69239d0e191eed14d03bf
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-15 22:20:21 -0800

    config: setting up some nice defaults from tim pope

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -4,3 +4,11 @@ vim.opt.number = true
 -- stuff for coc compatibility
 vim.opt.updatetime = 300
 vim.opt.signcolumn = 'yes'
+
+-- Save undo history
+vim.o.undofile = true
+vim.wo.number = true
+
+-- Case insensitive searching unless /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
