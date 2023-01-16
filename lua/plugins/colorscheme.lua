63645de44ea28450416799b82a03431d97288f16
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-15 22:42:54 -0800

    plugin: expanded treesitter configurations

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,28 @@
 return {
 	"ellisonleao/gruvbox.nvim",
-	{"nvim-treesitter/nvim-treesitter", tag = "v0.8.1"},
+	{
+		"nvim-treesitter/nvim-treesitter", tag = "v0.8.1",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"help",
+				"html",
+				"json", 
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"regex",
+				"vim",
+				"yaml",
+				"julia"
+			},
+			context_commentstring = { enable = true, enable_autocmd = false },
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
+	},
 	{"folke/zen-mode.nvim", config = function()
 	    require("zen-mode").setup {}
 	  end
