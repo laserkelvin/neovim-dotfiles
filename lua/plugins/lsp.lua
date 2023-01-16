f62da5c4463825a395036af50cfae4e0cf9a82a4
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-16 08:13:49 -0800

    plugins: added lsp-notify

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,10 @@
 return {
 	{"neoclide/coc.nvim", tag = "v0.0.81", pin = true},
 	{"fannheyward/coc-pyright", build = "npm install -g pyright"},
+	{"mrded/nvim-lsp-notify", config = function()
+		require("lsp-notify").setup({
+			notify = require("notify"),
+		})
+	end
+},
 }
