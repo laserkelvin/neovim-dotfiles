08ebf874ad44d30ba1a477e3ea8eeac5f15deb09
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-16 09:59:45 -0800

    config: added autocmd for closing certain buffers with just q

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,3 +1,21 @@
 
 local black_fmt = vim.api.nvim_create_augroup('black_fmt', {clear = true})
 
+-- Allows certain file types to be closed with q; grabbed from LazyVim
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"qf",
+		"help",
+		"man",
+		"notify",
+		"lspinfo",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true})
+	end,
+})
+
