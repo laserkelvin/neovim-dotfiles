317a4225b9a41f23e973293bbbf205bd6101f708
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-16 10:07:45 -0800

    config: first bit of code from notify for coc
    Taken code from [usage
    recipes](https://github.com/rcarriga/nvim-notify/wiki/Usage-Recipes)
    from the nvim-notify wiki.
    
    Haven't finished porting stuff over, but at least doesn't break

--- lua/config/coc.lua
+++ lua/config/coc.lua
@@ -21,3 +21,34 @@ vim.api.nvim_create_autocmd("CursorHold", {
 	command = "silent call CocActionAsync('highlight')",
 	desc = "Highlight symbol under cursor on CursorHold"
 })
+
+-- Hook in with nvim-notify
+local coc_status_record = {}
+
+function coc_status_notify(msg, level)
+	local notify_opts = { title = "CoC Status", timeout = 500, hide_from history = true, on_close = reset_coc_status_record }
+	if coc_status_record ~= {} then
+		notify_opts["replace"] = coc_status_record.id
+	end
+	coc_status_record = vim.notify(msg, level, notify_opts)
+end
+
+function reset_coc_status_record(window)
+	coc_status_record = {}
+end
+
+local coc_diag_record = {}
+
+function coc_diag_notify(msg, level)
+	local notify_opts = { title = "CoC Diagnostics", timeout = 500, on_close = reset_coc_diag_record }
+	if coc_diag_record ~= {} then
+		notify_opts["replace"] = coc_diag_record.id
+	end
+	coc_diag_record = vim.notify(msg, level, notify_opts)
+end
+
+function reset_coc_diag_record(window)
+	coc_diag_record = {}
+end
+
+
