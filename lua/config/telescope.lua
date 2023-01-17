20094e25fdcbc9f93045305a467ba70249be4eb5
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-01-17 09:07:13 -0800

    plugins: removing frecency for machines without sql
    Some machines won't have root access to install `sqlite3` and `libsqlite3-dev`
    which are dependencies for the `frecency` plugin. You'll live :)

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -2,5 +2,4 @@
 require("telescope").load_extension "file_browser"
 require("telescope").load_extension "lazy"
 require("telescope").load_extension "project"
-require("telescope").load_extension "frecency"
 require("telescope").load_extension "notify"
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,6 @@ return {
 	"kkharji/sqlite.lua",
 	"nvim-telescope/telescope-fzf-native.nvim",
 	"nvim-telescope/telescope-project.nvim",
-	"nvim-telescope/telescope-frecency.nvim",
 	"nvim-telescope/telescope-file-browser.nvim",
 	"tsakirist/telescope-lazy.nvim",
 	"olacin/telescope-cc.nvim",
