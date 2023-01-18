ee8ce6ef458c971287ac217b102f96f2d77c141c
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-17 20:37:37 -0800

    refactor: seeparating ui and misc plugins
    Organizationally, ui and misc plugins were joined and didn't really make
    sense. Most of the plugins contained in misc before were also
    ui-oriented.

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,9 @@
+return {
+    "rcarriga/nvim-notify",
+    "romgrk/barbar.nvim",
+    "kyazdani42/nvim-web-devicons",
+    {"nvim-lualine/lualine.nvim", options = { icons_enabled = false, theme = "auto"}},
+    {"mhinz/vim-startify", branch = "master"},
+    "MunifTanjim/nui.nvim",
+    "folke/noice.nvim"
+    }
