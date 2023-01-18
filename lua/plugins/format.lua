24ee5046bd7b263ebf3eab2eac4e353c18c1f423
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-01-17 20:55:09 -0800

    style: replacing tabs with spaces

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,10 +1,10 @@
 return {
-	"scrooloose/nerdcommenter",
-	"junegunn/vim-easy-align",
-	"sbdchd/neoformat",
-	"neomake/neomake",
-	"kylechui/nvim-surround",
-	{"windwp/nvim-autopairs", enable_check_bracket_line = false, ignored_nexty_char = "[%w%.]"},
-	{"kkoomen/vim-doge", build = ":call doge#install()"},
-	"lukas-reineke/indent-blankline.nvim"
+    "scrooloose/nerdcommenter",
+    "junegunn/vim-easy-align",
+    "sbdchd/neoformat",
+    "neomake/neomake",
+    "kylechui/nvim-surround",
+    {"windwp/nvim-autopairs", enable_check_bracket_line = false, ignored_nexty_char = "[%w%.]"},
+    {"kkoomen/vim-doge", build = ":call doge#install()"},
+    "lukas-reineke/indent-blankline.nvim"
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,10 @@
 return {
-	{"neoclide/coc.nvim", tag = "v0.0.82", pin = true},
-	{"fannheyward/coc-pyright", build = "npm install -g pyright"},
-	{"mrded/nvim-lsp-notify", config = function()
-		require("lsp-notify").setup({
-			notify = require("notify"),
-		})
-	end
+    {"neoclide/coc.nvim", tag = "v0.0.82", pin = true},
+    {"fannheyward/coc-pyright", build = "npm install -g pyright"},
+    {"mrded/nvim-lsp-notify", config = function()
+       require("lsp-notify").setup({
+          notify = require("notify"),
+       })
+    end
 },
 }
