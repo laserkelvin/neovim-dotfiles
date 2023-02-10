9e8bb47054c4453f2bbd1e675f8c47d37141018f
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-02-09 21:07:59 -0800

    fix: enabling telescope projects

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,6 +1,6 @@
 
 require("telescope").load_extension "file_browser"
 require("telescope").load_extension "lazy"
-require("telescope").load_extension "project"
+require("telescope").load_extension "projects"
 require("telescope").load_extension "frecency"
 require("telescope").load_extension "notify"
