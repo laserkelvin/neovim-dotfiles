1f43d5c03ed21a88cd3b98592a1e0b2954b7f6f0
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-02-13 21:53:48 -0800

    bindings: ported barbar bindings to lua

--- lua/config/bindings.lua
+++ lua/config/bindings.lua
@@ -46,42 +46,18 @@ map.set("n", "<leader>tr", ":NvimTreeToggle<CR>", { silent = true, noremap = tru
 -- aerial.nvim open
 map.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 
--- barbar; too lazy to port this to lua lol
-vim.cmd [[
-	nnoremap <silent>    <A-,> <Cmd>BufferPrevious<CR>
-	nnoremap <silent>    <A-.> <Cmd>BufferNext<CR>
-	" Re-order to previous/next
-	nnoremap <silent>    <A-<> <Cmd>BufferMovePrevious<CR>
-	nnoremap <silent>    <A->> <Cmd>BufferMoveNext<CR>
-	" Goto buffer in position...
-	nnoremap <silent>    <A-1> <Cmd>BufferGoto 1<CR>
-	nnoremap <silent>    <A-2> <Cmd>BufferGoto 2<CR>
-	nnoremap <silent>    <A-3> <Cmd>BufferGoto 3<CR>
-	nnoremap <silent>    <A-4> <Cmd>BufferGoto 4<CR>
-	nnoremap <silent>    <A-5> <Cmd>BufferGoto 5<CR>
-	nnoremap <silent>    <A-6> <Cmd>BufferGoto 6<CR>
-	nnoremap <silent>    <A-7> <Cmd>BufferGoto 7<CR>
-	nnoremap <silent>    <A-8> <Cmd>BufferGoto 8<CR>
-	nnoremap <silent>    <A-9> <Cmd>BufferGoto 9<CR>
-	nnoremap <silent>    <A-0> <Cmd>BufferLast<CR>
-	" Pin/unpin buffer
-	nnoremap <silent>    <A-p> <Cmd>BufferPin<CR>
-	" Close buffer
-	nnoremap <silent>    <A-c> <Cmd>BufferClose<CR>
-	" Wipeout buffer
-	"                          :BufferWipeout
-	" Close commands
-	"                          :BufferCloseAllButCurrent
-	"                          :BufferCloseAllButPinned
-	"                          :BufferCloseAllButCurrentOrPinned
-	"                          :BufferCloseBuffersLeft
-	"                          :BufferCloseBuffersRight
-	" Magic buffer-picking mode
-	nnoremap <silent> <C-p>    <Cmd>BufferPick<CR>
-	" Sort automatically by...
-	nnoremap <silent> <Space>bb <Cmd>BufferOrderByBufferNumber<CR>
-	nnoremap <silent> <Space>bd <Cmd>BufferOrderByDirectory<CR>
-	nnoremap <silent> <Space>bl <Cmd>BufferOrderByLanguage<CR>
-	nnoremap <silent> <Space>bw <Cmd>BufferOrderByWindowNumber<CR>
-]]
-
+-- barbar
+map.set("n", "<A-,>", ":BufferPrevious<CR>", { silent = true, noremap = true})
+map.set("n", "<A-.>", ":BufferNext<CR>", { silent = true, noremap = true})
+map.set("n", "<A-1>", ":BufferGoto 1<CR>", { silent = true, noremap = true})
+map.set("n", "<A-2>", ":BufferGoto 2<CR>", { silent = true, noremap = true})
+map.set("n", "<A-3>", ":BufferGoto 3<CR>", { silent = true, noremap = true})
+map.set("n", "<A-4>", ":BufferGoto 4<CR>", { silent = true, noremap = true})
+map.set("n", "<A-5>", ":BufferGoto 5<CR>", { silent = true, noremap = true})
+map.set("n", "<A-6>", ":BufferGoto 6<CR>", { silent = true, noremap = true})
+map.set("n", "<A-7>", ":BufferGoto 7<CR>", { silent = true, noremap = true})
+map.set("n", "<A-8>", ":BufferGoto 8<CR>", { silent = true, noremap = true})
+map.set("n", "<A-9>", ":BufferGoto 9<CR>", { silent = true, noremap = true})
+map.set("n", "<A-0>", ":BufferLast<CR>", { silent = true, noremap = true})
+map.set("n", "<A-c>", ":BufferClose<CR>", { silent = true, noremap = true})
+map.set("n", "<A-C>", ":BufferCloseAllButCurrent<CR>", { silent = true, noremap = true})
