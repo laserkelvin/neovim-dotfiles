893ddb1962a28ed75973577994fe1f2385f939d4
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2024-01-01 10:05:35 -0800

    theme: using catppuccin as colorscheme now
    Signed-off-by: Kelvin Lee <kin.long.kelvin.lee@gmail.com>

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = { flavour = "macchiato" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
