11e61780e21a2f7ff0ab31654dccffc501827167
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2024-01-03 10:17:01 -0800

    added keymaps for telekasten normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,4 +4,8 @@
 
 vim.keymap.set("n", "<Leader>d", "<cmd>DogeGenerate numpy<cr>")
 vim.keymap.set("n", "<Leader>tk", "<cmd>Telekasten panel<cr>")
+vim.keymap.set("n", "<Leader>tg", "<cmd>Telekasten search_notes<cr>")
+vim.keymap.set("n", "<Leader>tz", "<cmd>Telekasten follow_link<cr>")
+vim.keymap.set("n", "<Leader>tn", "<cmd>Telekasten new_note<cr>")
+vim.keymap.set("n", "<Leader>tb", "<cmd>Telekasten show_backlinks<cr>")
 vim.keymap.set("n", "<Leader>zm", "<cmd>ZenMode<cr>")
