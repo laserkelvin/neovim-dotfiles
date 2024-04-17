80fae662ee4bff7e97e104b7740fc2a4ee845e2c
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2024-04-16 22:47:11 -0700

    removing llm config from lazy.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -54,5 +54,3 @@ require("telekasten").setup({
   template_new_weekly = config_root .. "/" .. "templates/weekly.md",
   templates = config_root .. "/" .. "templates/",
 })
-
-require("nvim-llama").setup({ model = "deepseek-coder:6.7b" })
