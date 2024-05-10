944510c07dcd5a221d530193b41d4c0b5062f6a7
Lee, Kin Long Kelvin <kin.long.kelvin.lee@gmail.com>
2024-05-10 08:07:34 -0700

    added markdown preview and keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,6 @@ vim.keymap.set("n", "<Leader>x", "*``cgn")
 
 -- emojis and symbols
 vim.keymap.set("n", "<Leader>em", "<cmd>Telescope symbols<cr>")
+
+-- markdown preview
+vim.keymap.set("n", "<Leader>pr", "<cmd>MarkdownPreview<cr>")
