a79e995f1f5cb4b6e3b7674a865a329ec750be33
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2024-12-29 21:21:37 -0800

    added oil to plugins

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -35,5 +35,14 @@ return {
         }
       }
     }
+  },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = {
+      { "echasnovski/mini.icons", opts = {} },
+    }
   }
 }
