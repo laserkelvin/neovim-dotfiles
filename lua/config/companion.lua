6eb662365bbdc3e6a9ac2b155c78a24b3ab629a2
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2025-01-12 21:07:40 -0800

    feat: added pytest workflow

--- lua/config/companion.lua
+++ lua/config/companion.lua
@@ -157,6 +157,95 @@ require("codecompanion").setup(
           },
         },
       },
+      ["Make PyTest"] = {
+        strategy = "workflow",
+        description = "Multi-step workflow for generating unit tests with PyTest.",
+        opts = {
+          modes = { "v" },
+          short_name = "code_reason",
+          auto_submit = true,
+          stop_context_insertion = true,
+          user_prompt = true,
+        },
+        prompts = {
+          {
+            {
+              role = config.constants.SYSTEM_ROLE,
+              content = function(context)
+                return "### First step:\n\nYou are a principal engineer with many years developing with the Python"
+                  .. " programming language. You have been tasked to write unit tests for some code using PyTest,"
+                  .. " which we will break up the process into two steps. This is the first step, in which you"
+                  .. " will try and infer high-level characteristics about the code, focusing on the following:"
+                  .. " what variables are declared, the names of functions and classes, and any branching/flow"
+                  .. " control structures are present. The six questions you will answer are provided **after** the"
+                  .. " code: your response must follow the same structure as the questions, and you must attempt"
+                  .. " to answer every question. If you are unsure about something, do not make it up and instead"
+                  .. " ask for clarification, or say you do not know."
+              end,
+            },
+            {
+              role = config.constants.USER_ROLE,
+              content = function(context)
+                local text = actions.get_code(context.start_line, context.end_line)
+
+                return "### Code to generate unit tests for:\n\n"
+                  .. "```" .. context.filetype .. "\n"
+                  .. text
+                  .. "```\n"
+                  .. "\n### Questions:\n\n"
+                  .. "1. What variables are declared in the code? Variables are generally on the left side of statements with an `=` symbol."
+                  .. " An example of this would be `a = 2 + sin(c)`, where `a` is the variable being declared and implicitly assumes that"
+                  .. " the variable `c` has already been declared.\n"
+                  .. "2. What, if any, flow/branching statements are used in the code? An example of such a statement would be a `for`"
+                  .. " loop, or a conditional block would contain `if` and optionally `else` statements. If there are no such statements,"
+                  .. " say there are none.\n"
+                  .. "3. What, if any, is the name of the functions and/or class in the provided code? If there are no functions or classes"
+                  .. " in the code block, say there are none.\n"
+                  .. "4. If there are functions, what, if anything, are the arguments and keyword arguments that are accepted? List them"
+                  .. " out by their name(s). If there are neither arguments nor keyword arguments, or there are no functions, say there are none.\n"
+                  .. "5. If there are functions, what, if anything, is/are being returned by each function? Are they variables, or are they literals?"
+                  .. " If a function ends without a return statement, say that the corresponding function returns none.\n"
+                  .. "6. Reabstract or transcribe the provided code into pseudocode that distills the core algorithm and logic. In your pseudocode"
+                  .. " you should use simple language to express the logic."
+              end,
+              opts = {
+                contains_code = false,
+                -- override the default system prompt with our custom one
+                ignore_system_prompt = true,
+                }
+              }
+            },
+          {
+            {
+              role = config.constants.USER_ROLE,
+              content = function(context)
+                return "### Second step:\n\nFrom your answers above, implement one or multiple unit tests written"
+                .. " in `pytest` to ensure that the code functions as intended. If you are unsure"
+                .. " of the intention behind the code or cannot infer how it should be used, you"
+                .. " must ask for clarification. You are encouraged to use `@pytest.parameterize`"
+                .. " and other patterns to make the unit test(s) concise yet cover many different"
+                .. " cases without repetition. The unit tests should cover the following:\n"
+                .. "1. Use `@pytest.parameterize` to test different values for arguments; try different"
+                .. " numbers for numeric arguments, or different text lengths for strings.\n"
+                .. "2. Include both positive and negative tests: such as instances where exceptions"
+                .. " are expected to be triggered.\n"
+                .. "3. You should test the holistic behavior of functions if multiple are provided."
+                .. " This means you should include tests that test each independent function in isolation,"
+                .. " but also include tests that use functions together.\n"
+                .. "4. For each test, include docstrings that explain the behavior and intention of the test."
+                .. " The docstrings should be sufficiently comprehensive as to let anyone understand why the"
+                .. " test exists, and how it works.\n"
+                .. "5. Where appropriate, you should consider using the `hypothesis` library to mock data inputs."
+              end,
+              opts = {
+                contains_code = true,
+                auto_submit = true,
+                ignore_system_prompt = true,
+              }
+            }
+          },
+        }
+      },
       ["Code reasoning"] = {
         strategy = "workflow",
         description = "Reason about the code you have been given.",
