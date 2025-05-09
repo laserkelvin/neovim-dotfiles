859fd6d93a0dae3aca647b76db17b99a1bb67b26
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2025-05-09 10:37:35 -0700

    refactor: moving flash into productivity

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -1,7 +1,7 @@
 return {
   {
     "tris203/precognition.nvim",
-    config = {
+    opts = {
       startVisible = true,
       showBlankVirtLine = true,
     },
@@ -44,5 +44,19 @@ return {
     dependencies = {
       { "echasnovski/mini.icons", opts = {} },
     }
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   }
 }
